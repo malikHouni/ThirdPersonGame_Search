b9cb59a2009e6918dea0e69210c2b13b75f66782
malik houni <malik.houni@gmail.com>
2021-12-03 12:35:48 +0100

    Update TPSSeeker.py
    changed...

--- TPSSeeker.py
+++ TPSSeeker.py
@@ -1,56 +1,14 @@
 import streamlit as st
-# Library for opening url and creating
-# requests
-import urllib.request
 
-# pretty-print python data structures
-from pprint import pprint
-
-# for parsing all the tables present
-# on the website
-from html_table_parser.parser import HTMLTableParser
 
 # for converting the parsed data in a
 # pandas dataframe
 import pandas as pd
 
 
-# Opens a website and read its
-# binary contents (HTTP Response Body)
-def url_get_contents(url):
+df = pd.read_csv("./tpsList.csv")
+df= df.drop(['Unnamed: 0'],axis=1)
 
-	# Opens a website and read its
-	# binary contents (HTTP Response Body)
-
-	#making request to the website
-	req = urllib.request.Request(url=url)
-	f = urllib.request.urlopen(req)
-
-	#reading contents of the website
-	return f.read()
-
-# defining the html contents of a URL.
-xhtml = url_get_contents('https://en.wikipedia.org/wiki/List_of_third-person_shooters').decode('utf-8')
-
-# Defining the HTMLTableParser object
-p = HTMLTableParser()
-
-# feeding the html contents in the
-# HTMLTableParser object
-p.feed(xhtml)
-
-# Now finally obtaining the data of
-# the table required
-#pprint(p.tables[1])
-
-# converting the parsed data to
-# dataframe
-print("\n\nPANDAS DATAFRAME\n")
-df=pd.DataFrame(p.tables[1])
-
-#the first row is actually the columns, let's arrange that with following lines
-df.columns = df.iloc[0] 
-df = df[1:]
 df['Platform(s)']=df['Platform(s)'].str.split(',')
 
 st.header("Find the Third-Person-Shooter by platform:")
